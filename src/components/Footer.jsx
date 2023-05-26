c82c1a4a540b34a96ef4d525ba9b144b41e4e18e
Sanjay-acharya-au50 <111633827+Sanjay-acharya-au50@users.noreply.github.com>
2023-05-27 00:25:27 +0530

    commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='text-center text-white mt-3 bg-black'>
+        © Copyright TAC apply.
+    </footer>
+  )
+}
+
+export default Footer
